68d8fd0c68933300fb12a213af758f1cda3ed4f0
Robson Oliveira <ropj@cin.ufpe.br>
2023-07-27 16:35:17 -0300

    add: project icons

---
+++ src/components/images/outlined-close-icon.tsx
@@ -0,0 +1,9 @@
+const OutlinedCloseIcon = ({ className }: { className?: string }) => {
+    return (
+        <svg className={className} height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg">
+            <path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"/>
+        </svg>
+    )
+}
+
+export default OutlinedCloseIcon
---
+++ src/components/images/triangle.tsx
@@ -0,0 +1,9 @@
+const TriangleImage = ({ className }: { className?: string }) => {
+    return (
+        <svg className={className} width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path id="Polygon 1" d="M13 16.5L0.875648 0.75L25.1244 0.75L13 16.5Z" fill=""/>
+        </svg>
+    )
+}
+
+export default TriangleImage
