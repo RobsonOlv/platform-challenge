38511c38663060d1352b5b30fb917a884bcffa51
Robson Oliveira <ropj@cin.ufpe.br>
2023-07-28 11:50:22 -0300

    update: footer and header links

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,22 +1,22 @@
+import WhiteValcannLogo from 'components/images/Valcann-white.svg'
 import styles from './styles.module.css'
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div>
-                <nav>
-                    <ul className={styles.footerLinks}>
-                        <li>Terms of Service</li>
-                        <div className={styles.separator}/>
-                        <li>Contact</li>
-                        <div className={styles.separator}/>
-                        <li>Privacy policy</li>
-                    </ul>
-                </nav>
-                <div className={styles.rightsContainer}>
-                    <div style={{ width: 32, height: 32, background: 'red' }}></div>
-                    <p>All rights reserved.</p>
-                </div>
+            <nav>
+                <ul className={styles.footerLinks}>
+                    <li>Terms of Service</li>
+                    <div className={styles.separator}/>
+                    <li>Contact</li>
+                    <div className={styles.separator}/>
+                    <li>Privacy policy</li>
+                </ul>
+            </nav>
+            <div className={styles.rightsContainer}>
+                <a target='_blank' rel="noopener noreferrer"  href="https://www.valcann.com.br/">
+                    <img src={WhiteValcannLogo} alt="Logo branca da Valcann" />
+                </a>
             </div>
         </footer>
     )
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,9 @@ import styles from './styles.module.css'
 const Header = () => {
     return (
         <header className={styles.header}>
-            <div style={{ width: 32, height: 32, background: 'red' }}></div>
+            <a target='_blank' rel="noopener noreferrer" href='https://www.valcann.com.br/'>
+                <img className={styles.logo} src="https://cdn-images-1.medium.com/v2/resize:fit:724/1*mRme0B25xZ00nxR8W8_ECg@2x.png"  alt="Logo colorido da Valcann" />
+            </a>
             <nav className={styles.rightLinksContainer}>
                 <ul className={styles.rightLinks}>
                     <li>Quem somos?</li>
