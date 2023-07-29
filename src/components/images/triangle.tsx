19cc26dbe17ebe48ebd7617e740fed9b0a39b069
Robson Oliveira <ropj@cin.ufpe.br>
2023-07-29 00:22:11 -0300

    add: navigation icons and form icons

--- src/components/images/triangle.tsx
+++ src/components/images/triangle.tsx
@@ -1,6 +1,6 @@
-const TriangleImage = ({ className }: { className?: string }) => {
+const TriangleImage = ({ className, fill }: { className?: string, fill: string }) => {
     return (
-        <svg className={className} width="26" height="17" viewBox="0 0 26 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className={className} width="26" height="17" viewBox="0 0 26 17" fill={fill} xmlns="http://www.w3.org/2000/svg">
             <path id="Polygon 1" d="M13 16.5L0.875648 0.75L25.1244 0.75L13 16.5Z" fill=""/>
         </svg>
     )
