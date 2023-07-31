f0a58a1b801ad1d085e504712d1cfdf720682298
Robson Oliveira <ropj@cin.ufpe.br>
2023-07-30 21:52:46 -0300

    update: landing page texts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,14 +8,21 @@ const HomePage = () => {
             <section id="about" className={styles.aboutContainer}>
                 <CarouselSection />
                 <div className={styles.aboutDescriptionContainer}>
-                    <h1 className={styles.aboutDescriptionTitle}>Tenha a melhor experiência do mercado com a excelência <span>BLABLA</span></h1>
-                    <p>Lorem ipsum blablaLorem ipsum blablaLorem ipsum blabla</p>
+                    <h1 className={styles.aboutDescriptionTitle}>Tenha a melhor experiência do mercado com a excelência <span>Valcann</span></h1>
+                    <p>A Valcann é especialista em Computação em Nuvem. Nossa prática é baseada no amplo conhecimento da nossa equipe, a qual além de larga experiência profissional e técnica, é fortemente envolvida com a academia.</p>
                 </div>
             </section>
             <TrailsSection />
             <section id="info" className={styles.infoContainer}>
-                <h2>Impulsione seu modelo</h2>
-                <text>Talvez uma trilha</text>
+                <div className={styles.infoBox}>
+                    <div className={styles.infoDescriptionContainer}>
+                        <h2>Impulsione sua empresa</h2>
+                        <p>
+                            Planejamento, execução e otimização de estratégias de migração para a Nuvem, com construção de jornada de adoção de Nuvem, plano de execução e migração.
+                        </p>
+                    </div>
+                    <img src="https://lh3.googleusercontent.com/U8XJwjwCg1w6awmeS7USpkosIwSuQnAbYx61dGJ4f7UKcZvWKJpoMYsbOuxO4jc-B9F2TjoApGVpDcROA5Dg3n2vEeYJAF9RJ_xr8pnVMlu0fa-v-wLb4Ooq4k7rbIuOKg=w1280" alt="Imagem de trilha de migração" />
+                </div>
             </section>
         </div>
     )
